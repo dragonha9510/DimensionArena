5d3e50b1c2333cc7885a16743ed5eff6d8ab4eb1
이혜원 <mvat1225@gmail.com>
2022-09-21 15:46:51 +0900

    JSB_20220921
    15:46서버 연동

---
+++ DimensionArena_URP_Photon/Assets/01.Scripts/JSB_Script/LobbyManager.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class LobbyManager : MonoBehaviourPunCallbacks
+{
+    // 시작 플레이어 제한 수 입니다
+    [SerializeField]
+    int startPlayerCount = 4;
+    // 현 접속한 플레이어의 카운트 입니다
+    public int connectCount = 0;
+    // 이름 InputField Text 입니다
+    [SerializeField]
+    TextMeshProUGUI nameText;
+    
+    [SerializeField]
+    TextMeshProUGUI connectText;
+
+
+    int roomCount = 0;
+    string defaultRoomName = "Room";
+
+
+    private void Awake()
+    {
+        Screen.SetResolution(1920, 1080, false);
+    }
+    public void Connect()
+    {
+        Debug.Log("Connect");
+        PhotonNetwork.LocalPlayer.NickName = nameText.text;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("OnConnectedToMaster");
+        
+        PhotonNetwork.JoinOrCreateRoom("Room"+ roomCount.ToString(), new RoomOptions { MaxPlayers = 2 },null);
+    }
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("접속 실패");
+        ++roomCount;
+        PhotonNetwork.JoinOrCreateRoom("Room" + roomCount.ToString(), new RoomOptions { MaxPlayers = 2 }, null);
+      
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom");
+        
+        if (startPlayerCount == PhotonNetwork.CurrentRoom.PlayerCount)
+            GetComponent<PhotonView>().RPC("LoadingInGame", RpcTarget.All);
+        else if (PhotonNetwork.IsConnected)
+        {
+            connectCount = PhotonNetwork.CurrentRoom.PlayerCount;
+            connectText.GetComponent<ConnectCountText>().RefreshServerText(connectCount);
+        }
+    }
+
+    [PunRPC]
+    private void LoadingInGame()
+    {
+        PhotonNetwork.LoadLevel("ProtoType");
+
+    }
+}
