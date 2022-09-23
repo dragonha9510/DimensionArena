615c263dd15c7996d330129ac25e91ef4044cb3d
이혜원 <mvat1225@gmail.com>
2022-09-23 16:38:51 +0900

    20220923
    자기장 생성 로직 설계 완료

--- DimensionArena_URP_Photon/Assets/01.Scripts/JSB_Script/MagneticField.cs
+++ DimensionArena_URP_Photon/Assets/01.Scripts/JSB_Script/MagneticField.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class MagneticField : MonoBehaviour
 {
+    [SerializeField] float decreaseTime = 10.0f;
+
     [SerializeField] Vector2 magneticfieldScale;
 
-    private Vector2 magneticfieldpos;
 
     [SerializeField] Transform leftTopGround;
     [SerializeField] Transform leftBottomGround;
@@ -17,31 +18,52 @@ public class MagneticField : MonoBehaviour
 
     [SerializeField] private float scaleSpeed = 1f;
 
+    
+    private float leftCorrection;
+    private float rightCorrection;
+    private float topCorrection;
+    private float bottomCorrection;
+
     private List<GameObject> magneticfieldObj = new List<GameObject>();
+    private Vector2 magneticfieldpos;
+
     private void SettingRandomPosition()
     {
-        // 우선 임시로 중앙값으로 설정을 시켜 놓자.
-        magneticfieldpos.x = leftTopGround.position.x - rightTopGround.position.x / 2;
-        magneticfieldpos.y = leftTopGround.position.y - rightTopGround.position.y / 2;
+        Vector2 minLeftBottom;
+        Vector2 maxRightTop;
+        // 범위의 최솟값을 구하기 위해 우선은 자기장 시작점 중 left , bottom 값을 넣는다 .
+        minLeftBottom.x = leftBottomGround.position.x;
+        minLeftBottom.y = leftBottomGround.position.z;
 
-        //Vector2 minLeftBottom;
-        //Vector2 maxRightTop;
-        //// 범위의 최솟값을 구하기 위해 우선은 자기장 시작점 중 left , bottom 값을 넣는다 .
-        //minLeftBottom.x = leftBottomGround.position.x;
-        //minLeftBottom.y = leftBottomGround.position.z;
-        //
-        //// 범위의 최대값을 구하기 위해 우선은 자기장 시작점 중 right , top 값을 넣는다 .
-        //maxRightTop.x = rightTopGround.position.x;
-        //maxRightTop.y = rightTopGround.position.z;
-        //
-        //// 범위의 최솟값을 자기장 영역의 반을 더해서 보정한다.
-        //minLeftBottom.x += magneticfieldScale.x / 2;
-        //minLeftBottom.y += magneticfieldScale.y / 2;
-        //
-        //// 범위의 최대값을 자기장 영역의 반을 빼서 보정한다.
-        //maxRightTop.x -= magneticfieldScale.x / 2;
-        //maxRightTop.y -= magneticfieldScale.y / 2;
+        // 범위의 최대값을 구하기 위해 우선은 자기장 시작점 중 right , top 값을 넣는다 .
+        maxRightTop.x = rightTopGround.position.x;
+        maxRightTop.y = rightTopGround.position.z;
 
+        // 범위의 최솟값을 자기장 영역의 반을 더해서 보정한다.
+        minLeftBottom.x += magneticfieldScale.x / 2;
+        minLeftBottom.y += magneticfieldScale.y / 2;
+
+        // 범위의 최대값을 자기장 영역의 반을 빼서 보정한다.
+        maxRightTop.x -= magneticfieldScale.x / 2;
+        maxRightTop.y -= magneticfieldScale.y / 2;
+
+
+        magneticfieldpos.x = Random.Range(minLeftBottom.x, maxRightTop.x);
+        magneticfieldpos.y = Random.Range(minLeftBottom.y, maxRightTop.y);
+
+
+        // 자기장이 줄어들 시간에 맞춰서 비례해서 확장되야 함으로 1초에 확장될 양은 확장량 / 줄어들 초
+        leftCorrection = (magneticfieldpos.x - magneticfieldScale.x / 2) - leftTopGround.position.x;
+        leftCorrection = leftCorrection / decreaseTime;
+        
+        rightCorrection = rightBottomGround.position.x - (magneticfieldpos.x + magneticfieldScale.x / 2);
+        rightCorrection = rightCorrection / decreaseTime;
+        
+        topCorrection = leftTopGround.position.z - (magneticfieldpos.y + magneticfieldScale.y / 2);
+        topCorrection = topCorrection / decreaseTime;
+        
+        bottomCorrection = (magneticfieldpos.y - magneticfieldScale.y / 2) - leftBottomGround.position.z;
+        bottomCorrection = bottomCorrection / decreaseTime;
     }
 
     void SetEachOther(Transform start, Transform end)
@@ -82,7 +104,7 @@ public class MagneticField : MonoBehaviour
     void expansionMangeticField()
     {
         // 확대가 될 양
-        float expanstionValue = scaleSpeed * Time.deltaTime;
+        float expanstionValue = leftCorrection * Time.deltaTime;
         float correctionValue = magneticfieldObj[0].transform.localScale.x;
 
         //왼쪽 자기장면 확대
@@ -90,57 +112,56 @@ public class MagneticField : MonoBehaviour
                                                                 , magneticfieldObj[0].transform.localScale.y
                                                                 , magneticfieldObj[0].transform.localScale.z);
         //왼쪽 자기장면 보정
-        correctionValue -= magneticfieldObj[0].transform.localScale.x;
-        Mathf.Abs(correctionValue);
-        correctionValue /= 2;
-        magneticfieldObj[0].transform.localPosition = new Vector3(magneticfieldObj[0].transform.position.x - correctionValue
+        correctionValue = expanstionValue / 2;
+        Debug.Log("보정치 : " + correctionValue);
+        Debug.Log(magneticfieldObj[0].transform.localPosition);
+        magneticfieldObj[0].transform.localPosition = new Vector3(magneticfieldObj[0].transform.position.x + correctionValue
                                                                 , magneticfieldObj[0].transform.position.y
                                                                 , magneticfieldObj[0].transform.position.z);
 
         //오른쪽 자기장면 확대
+        expanstionValue = rightCorrection * Time.deltaTime;
         correctionValue = magneticfieldObj[1].transform.localScale.x;
         magneticfieldObj[1].transform.localScale = new Vector3(magneticfieldObj[1].transform.localScale.x + expanstionValue
                                                                 , magneticfieldObj[1].transform.localScale.y
                                                                 , magneticfieldObj[1].transform.localScale.z);
 
         //오른쪽 자기장면 보정
-        correctionValue -= magneticfieldObj[1].transform.localScale.x;
-        Mathf.Abs(correctionValue);
-        correctionValue /= 2;
-        magneticfieldObj[1].transform.localPosition = new Vector3(magneticfieldObj[1].transform.position.x + correctionValue
+        correctionValue = expanstionValue / 2;
+        magneticfieldObj[1].transform.localPosition = new Vector3(magneticfieldObj[1].transform.position.x - correctionValue
                                                                 , magneticfieldObj[1].transform.position.y
                                                                 , magneticfieldObj[1].transform.position.z);
 
 
         //상단 자기장면 확대
+        expanstionValue = topCorrection * Time.deltaTime;
+
         correctionValue = magneticfieldObj[2].transform.localScale.z;
         magneticfieldObj[2].transform.localScale = new Vector3(magneticfieldObj[2].transform.localScale.x
                                                                 , magneticfieldObj[2].transform.localScale.y
                                                                 , magneticfieldObj[2].transform.localScale.z + expanstionValue);
         //상단 자기장면 보정
-        correctionValue -= magneticfieldObj[2].transform.localScale.z;
-        Mathf.Abs(correctionValue);
-        correctionValue /= 2;
+        correctionValue = expanstionValue / 2;
         magneticfieldObj[2].transform.localPosition = new Vector3(magneticfieldObj[2].transform.position.x 
                                                                 , magneticfieldObj[2].transform.position.y
-                                                                , magneticfieldObj[2].transform.position.z + correctionValue);
+                                                                , magneticfieldObj[2].transform.position.z - correctionValue);
 
 
 
         //하단 자기장면 확대
+        expanstionValue = bottomCorrection   * Time.deltaTime;
+
         correctionValue = magneticfieldObj[3].transform.localScale.z;
         magneticfieldObj[3].transform.localScale = new Vector3(magneticfieldObj[3].transform.localScale.x
                                                                 , magneticfieldObj[3].transform.localScale.y
                                                                 , magneticfieldObj[3].transform.localScale.z + expanstionValue);
         //하단 자기장면 보정
-        correctionValue -= magneticfieldObj[3].transform.localScale.z;
-        Mathf.Abs(correctionValue);
-        correctionValue /= 2;
+        correctionValue = expanstionValue / 2;
         magneticfieldObj[3].transform.localPosition = new Vector3(magneticfieldObj[3].transform.position.x
                                                                 , magneticfieldObj[3].transform.position.y
-                                                                , magneticfieldObj[3].transform.position.z - correctionValue);
+                                                                , magneticfieldObj[3].transform.position.z + correctionValue);
 
-
+        decreaseTime -= Time.deltaTime;
     }
 
     void Start()
@@ -158,11 +179,21 @@ public class MagneticField : MonoBehaviour
         // 하단 면
         SetEachOther(rightBottomGround, leftBottomGround);
         magneticfieldObj.Add(Instantiate(magneticRect));
+
+        SettingRandomPosition();
     }
 
     void FixedUpdate()
     {
-
-        expansionMangeticField();
+        if (0f <= decreaseTime)
+            expansionMangeticField();
+        else
+        {
+            foreach(GameObject obj in magneticfieldObj)
+            {
+                Destroy(obj);
+            }
+            Destroy(this);
+        }
     }
 }
