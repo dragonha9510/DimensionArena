5a4609cf8898f0d3e9e5c13249d572ed7773c4e4
이혜원 <mvat1225@gmail.com>
2022-09-28 14:58:38 +0900

    20220928
    겜매 복구 및 UI 저장

--- DimensionArena_URP_Photon/Assets/01.Scripts/JSB_Script/GameManager.cs
+++ DimensionArena_URP_Photon/Assets/01.Scripts/JSB_Script/GameManager.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
+
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+using ExitGames.Client.Photon;
+
+public class GameManager : MonoBehaviourPunCallbacks, IPunObservable
 {
     private static GameManager m_instance;
     public static GameManager instance
     {
         get
         {
-            if(null == m_instance)
+            if (null == m_instance)
             {
                 m_instance = FindObjectOfType<GameManager>();
             }
@@ -25,4 +29,27 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
     }
 
+
+    public void OnEvent(EventData photonEvent)
+    {
+        if (0 == photonEvent.Code)
+        {
+            object[] data = (object[])photonEvent.CustomData;
+            for (int i = 0; i < data.Length; ++i)
+            {
+                print(data[i]);
+            }
+        }
+    }
+
+    private void Start()
+    {
+        Vector3 spawnPoint = new Vector3(0, 1, 0);
+        PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint, Quaternion.identity);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        throw new System.NotImplementedException();
+    }
 }
