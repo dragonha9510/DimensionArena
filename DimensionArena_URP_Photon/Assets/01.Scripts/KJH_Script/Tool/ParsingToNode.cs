036b1b1dac8048ba3c8b4768eb802d2b6f146e44
GrittyCoding <kanghan9830@gmail.com>
2022-09-28 17:48:38 +0900

    Tool System Complete
    Tool System Complete on 2022-09-28

---
+++ DimensionArena_URP_Photon/Assets/01.Scripts/KJH_Script/Tool/ParsingToNode.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParsingToNode : MonoBehaviour
+{
+    public Vector2Int idx;
+    public Rect rect;
+    public GRITTY.NodeInformation nodeInfo;
+    public bool brown;
+}
