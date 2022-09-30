0a50b2b8566c06207c74df5ae9f02ad3f62ddbbe
dragonha9510 <dragonha9510@gmail.com>
2022-09-30 14:51:01 +0900

    Decal_patch

---
+++ DimensionArena/Assets/Scripts/KYH_Script/LoadingScene.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class LoadingScene : MonoBehaviour
+{
+    [SerializeField] private TMP_Text text;
+    [SerializeField] private Image curGauge;
+    [SerializeField] private Image maxGauge;
+    private float maxWidth;
+    private float curWidth;
+    private float gaugeUnit;
+
+    private void Start()
+    {
+        maxWidth = maxGauge.rectTransform.sizeDelta.x;
+        curGauge.rectTransform.sizeDelta = new Vector2(0, curGauge.rectTransform.sizeDelta.y);
+
+        // 로드 해야할 총 크기 구하기.
+        // if ) maxWidth   =   1000
+        // ex ) 총 파일 수  =   1000 _ 파일 1 개당 curGauge 의 sizeDelta.x += 1f;
+        // if ) maxWidth   =   400
+        // ex ) 총 파일 수  =   400  _ 파일 1 개당 curGauge 의 sizeDelta.x += 2.5f;
+        // if ) maxWidth   =   1000
+        // ex ) 파일  크기  =   1000 .. 위와 동일
+        // gaugeUnit = ( maxWidth / 총 크기 );
+
+        StartCoroutine("loadData");
+    }
+
+    private void Update()
+    {
+        curGauge.rectTransform.sizeDelta = new Vector2(curWidth, curGauge.rectTransform.sizeDelta.y);
+    }
+
+    IEnumerator loadData()
+    {
+        // 로드된 단위에 맞추어 curWidth = gaugeUnit * 로드된 양;
+
+        // Test Code
+        text.text = "로딩 시작";
+        gaugeUnit = 10f;
+        yield return new WaitForSeconds(1.5f);
+        text.text = "로딩 1";
+        curWidth += gaugeUnit * 25f;
+        yield return new WaitForSeconds(1.5f);
+        text.text = "로딩 2";
+        curWidth += gaugeUnit * 25f;
+        yield return new WaitForSeconds(1.5f);
+        text.text = "로딩 3";
+        curWidth += gaugeUnit * 25f;
+        yield return new WaitForSeconds(1.5f);
+        text.text = "로딩 4";
+        curWidth += gaugeUnit * 25f;
+        yield return new WaitForSeconds(1.5f);
+
+        if (Mathf.Abs(curGauge.rectTransform.sizeDelta.x - maxGauge.rectTransform.sizeDelta.x) <= float.Epsilon)
+        {
+            // 씬 전환
+        }
+    }
+}
---
+++ DimensionArena/Assets/Scripts/KYH_Script/SceneChanger_Loading.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger_Loading : MonoBehaviour
+{
+    // 로딩 -> 메인
+    // 큐 -> 로딩 -> 인게임
+
+    private string curScene;
+    private string nextScene;
+
+    private string loaddata; // string which for load data path
+}
