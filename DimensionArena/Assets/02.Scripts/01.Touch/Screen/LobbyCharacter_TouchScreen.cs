6955f86aa8e12ecf4f9d31034beccdaa5fc31352
dragonha9510 <dragonha9510@gmail.com>
2022-10-05 20:11:38 +0900

    UI_Add
    Lobby_UI_Add

--- DimensionArena/Assets/02.Scripts/01.Touch/Screen/LobbyCharacter_TouchScreen.cs
+++ DimensionArena/Assets/02.Scripts/01.Touch/Screen/LobbyCharacter_TouchScreen.cs
@@ -3,29 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class LobbyCharacter_TouchScreen : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class LobbyCharacter_TouchScreen : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerDownHandler, IPointerUpHandler
 {
     [SerializeField] private Transform player;
     [SerializeField] private float rotationSpeed = 90.0f;
     private Vector3 oriRot;
     private Vector2 touchPos;
     private bool isTouch;
+    private float curTouchTime;
+    private float maxTouchTime;
     private bool once;
 
     private void Start()
     {
         oriRot = player.rotation.eulerAngles;
-        Debug.Log(oriRot);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        isTouch = true;
+    }
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (isTouch)
+        {
+            isTouch = false;
+            // Scene 실행
+            Debug.Log("Scene 전환!");
+        }
     }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         if (once)
-        {
-            StopCoroutine(RecoverRotation(0));
-        }
+            StopCoroutine("RecoverRotation");
+
+        isTouch = false;
         once = true;
-        isTouch = true;
         touchPos = eventData.position;
     }
 
@@ -38,30 +52,27 @@ public class LobbyCharacter_TouchScreen : MonoBehaviour, IBeginDragHandler, IDra
 
     public void OnEndDrag(PointerEventData eventData)
     {
-        isTouch = false;
-
         StartCoroutine("RecoverRotation", (Time.deltaTime));
     }
 
     IEnumerator RecoverRotation(float delta)
     {
-        while(!isTouch)
+        while(true)
         {
             float curY = player.rotation.y * Mathf.Rad2Deg;
 
             if (Mathf.Abs(curY - oriRot.y) <= rotationSpeed * delta)
-            {
                 yield break;
-            }
 
             float angle = player.rotation.eulerAngles.y < 180 ? -1 : 1;
+            float speedAccel = player.rotation.eulerAngles.y - (angle > 0 ? 360 : 0);
+            speedAccel = Mathf.Abs(speedAccel) / 90;
 
-            Debug.Log(angle);
+            player.Rotate(new Vector3(0, (angle * (rotationSpeed * speedAccel) * delta), 0));
 
-            player.Rotate(new Vector3(0, (angle * rotationSpeed * delta), 0));
             yield return null;
         }
-
-        yield break;
     }
+
+
 }
