49376875141eecad6e9fc6538fa38e5f4e812cfb
GrittyCoding <kanghan9830@gmail.com>
2022-10-05 14:46:33 +0900

    Player arrow Add
    Player arrow Add wiht server

--- DimensionArena/Assets/02.Scripts/50.Managers/PlayerUIManager.cs
+++ DimensionArena/Assets/02.Scripts/50.Managers/PlayerUIManager.cs
@@ -8,12 +8,18 @@ public class PlayerUIManager : MonoBehaviour
     Player target;
     [SerializeField] TextMeshProUGUI playerName;
     [SerializeField] Slider hpBarSlider;
-
+    [SerializeField] GameObject arrow;
+    [SerializeField] float arrowTime = 3.0f;
     void Start()
     {
+
         //플레이어 받아오기.
         target = GetComponentInParent<Player>();
 
+        if(target.photonView.IsMine)
+        {
+            arrow.SetActive(true);
+        }
         //환경 정보 분기점
         string nickName;
         nickName = 
@@ -24,8 +30,36 @@ public class PlayerUIManager : MonoBehaviour
 
         //Target
         target.Info.EcurHPChanged += HpBarChange;
+
+        StartCoroutine(ArrowMoveCoroutine());
+
     }
 
+    IEnumerator ArrowMoveCoroutine()
+    {
+       
+
+        //5번 흔들림
+        for(int i = 0; i < 5; ++i)
+        {
+            //상
+            for (int j = 0; j < 20; ++j)
+            {
+                arrow.transform.position += (Vector3.up * 0.025f);
+                yield return new WaitForSeconds(arrowTime * 0.005f);
+            }
+
+            //하
+            for (int j = 0; j < 20; ++j)
+            {
+                arrow.transform.position += (Vector3.down * 0.025f);
+                yield return new WaitForSeconds(arrowTime * 0.005f);
+            }
+        }
+
+        arrow.SetActive(false);
+
+    }
 
 
     void HpBarChange(float amount)
