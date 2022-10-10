8795663937ed9b60b8f81098b5094124e00bb22b
dragonha9510 <dragonha9510@gmail.com>
2022-10-10 16:47:27 +0900

    Player_BushEffect Fix

--- DimensionArena/Assets/02.Scripts/00.Player/AttackRange_Mesh/Atk_Parabola.cs
+++ DimensionArena/Assets/02.Scripts/00.Player/AttackRange_Mesh/Atk_Parabola.cs
@@ -2,7 +2,63 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshFilter))]
 public class Atk_Parabola : MonoBehaviour
 {
-    
+    Mesh mesh;
+    [SerializeField] private float meshWidth;
+
+    [SerializeField] private float velocity;
+    [SerializeField] private float angle;
+    [SerializeField] private int resolution = 10;
+
+    private float gravity; // force of gravity on the y axis
+    private float radianAngle;
+
+    private void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        gravity = Mathf.Abs(Physics.gravity.y);
+    }
+
+    private void OnValidate()
+    {
+        if (lineRenderer != null && Application.isPlaying)
+            RenderArc();
+    }
+
+    private void Start()
+    {
+        RenderArc();
+    }
+
+    void RenderArc()
+    {
+        lineRenderer.positionCount = (resolution + 1);
+        lineRenderer.SetPositions(CalculateArcArray());
+    }
+
+    Vector3[] CalculateArcArray()
+    {
+        Vector3[] arcArray = new Vector3[resolution + 1];
+
+        radianAngle = Mathf.Deg2Rad * angle;
+        float maxDistance = (velocity * velocity * Mathf.Sin(2 * radianAngle)) / gravity;
+
+        for(int i = 0; i <= resolution; ++i)
+        {
+            float t = (float)i / (float)resolution;
+            arcArray[i] = CalculateArcPoint(t, maxDistance);
+        }
+
+        return arcArray;
+    }
+
+    Vector3 CalculateArcPoint(float t, float distance)
+    {
+        float x = t * distance;
+        float y = x * Mathf.Tan(radianAngle) - ((gravity * x * x) / (2 * velocity * velocity * Mathf.Cos(radianAngle) * Mathf.Cos(radianAngle)));
+
+        return new Vector3(x, y);
+    }
 }
