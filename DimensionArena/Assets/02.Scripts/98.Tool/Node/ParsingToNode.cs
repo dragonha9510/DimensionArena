a161a1f3a14c6d8a666048a930e7fe14531944f6
GrittyCoding <kanghan9830@gmail.com>
2022-10-11 20:15:11 +0900

    MapTool add mode
    MapTool add mode on 2022-10-11

--- DimensionArena/Assets/02.Scripts/98.Tool/Node/ParsingToNode.cs
+++ DimensionArena/Assets/02.Scripts/98.Tool/Node/ParsingToNode.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using GRITTY;
 #if UNITY_EDITOR
+
 public class ParsingToNode : MonoBehaviour
 {
     public Vector2Int idx;
     public Rect rect;
-    public GRITTY.NodeInformation nodeInfo;
+    public NodeInformation nodeInfo;   
     public bool brown;
+
 }
 #endif
