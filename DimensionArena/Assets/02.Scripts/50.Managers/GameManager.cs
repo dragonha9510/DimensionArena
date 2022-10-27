dfbd9319435a35cd160de0d0320840109aaf7d5d
dragonha9510 <dragonha9510@gmail.com>
2022-10-27 14:33:49 +0900

    Alpha_LastCommit

--- DimensionArena/Assets/02.Scripts/50.Managers/GameManager.cs
+++ DimensionArena/Assets/02.Scripts/50.Managers/GameManager.cs
@@ -50,7 +50,7 @@ public class GameManager : MonoBehaviourPunCallbacks, IPunObservable
         GameMode = GAMEMODE.Survival;
 
 
-        Vector3 spawnPoint = new Vector3(5, 0, 5);
+        Vector3 spawnPoint = new Vector3(0, 0, 0);
 
         //   추 후 이거 유동적으로 바꿔야함. 게임매니저 문제점 기록하기
         
