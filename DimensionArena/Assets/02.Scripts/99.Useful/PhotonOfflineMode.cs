2b484297526d14f0bbcbaecaf451b7a7cd1f4e11
dragonha9510 <dragonha9510@gmail.com>
2022-11-07 12:21:38 +0900

    TickDamageUpdate
    + Ravagebell TickDamage Add
    + TickDamage OwnerID Add
    + Ravagebell Status Fix
    + JooHyeock Status Fix
    + Character Illustration Change
    + AttackObject Fix

--- DimensionArena/Assets/02.Scripts/99.Useful/PhotonOfflineMode.cs
+++ DimensionArena/Assets/02.Scripts/99.Useful/PhotonOfflineMode.cs
@@ -2,13 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using ManagerSpace;
 
 public class PhotonOfflineMode : MonoBehaviour
 {
     [SerializeField] private bool offlineMode;
+    private bool once;
 
     private void Awake()
     {
         PhotonNetwork.OfflineMode = offlineMode;
+    }
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if(!once)
+        {
+            PlayerInfoManager.Instance.RegisterPlayer();
+
+            once = true;
+        }
     }
 }
