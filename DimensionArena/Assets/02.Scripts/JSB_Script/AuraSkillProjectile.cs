f642d0c036e01bad81f0996e7209c30c79356d66
DEV_JSB <mvat1225@gmail.com>
2022-11-12 18:18:54 +0900

    20221112
    아우라 새로운 프로젝타일

---
+++ DimensionArena/Assets/02.Scripts/JSB_Script/AuraSkillProjectile.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AuraSkillProjectile : Projectile
+{
+    [SerializeField]
+    private GameObject explosionEffect;
+    [SerializeField]
+    private float createCycle = 0.3f;
+    private float timer = 0f;
+
+
+    private void Start()
+    {
+        StartCoroutine(CreateEffect());
+        timer = createCycle;
+    }
+
+    IEnumerator CreateEffect()
+    {
+        while(true)
+        {
+            timer += Time.deltaTime;
+            if(timer >= createCycle)
+            {
+                Instantiate(explosionEffect, this.transform);
+                timer = 0f;
+            }
+            yield return null;
+        }
+    }
+
+}
