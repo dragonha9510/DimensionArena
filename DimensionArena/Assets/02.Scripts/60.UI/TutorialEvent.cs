e60355799cb510198bd3e282fa55d0d8bd4afb5d
GrittyCoding <kanghan9830@gmail.com>
2022-11-15 15:30:55 +0900

    csv parsing
    csv parsing

--- DimensionArena/Assets/02.Scripts/60.UI/TutorialEvent.cs
+++ DimensionArena/Assets/02.Scripts/60.UI/TutorialEvent.cs
@@ -1,17 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TutorialEvent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public struct EventType
     {
+        public int idx;
+        public string dialogue;
+        public bool isCondition;        
     }
 
-    // Update is called once per frame
-    void Update()
+    List<Dictionary<string, object>> dialogue = new List<Dictionary<string, object>>();
+    [SerializeField] SerializableDictionary<int, string> dicDialogue = new SerializableDictionary<int, string>();
+    [SerializeField] TextMeshProUGUI curText;
+    [SerializeField] CanvasGroup touchBlockGroup;
+
+    [SerializeField] List<BaseEvent> listEvent = new List<BaseEvent>();
+    BaseEvent curEvent;
+    public SerializableDictionary<int, string> DicDialogue => dicDialogue;
+
+    void Awake()
     {
-        
+        dialogue = CSVReader.Read("Log/Tutorial");
     }
+
+
+    private void Update()
+    {
+        if (listEvent.Count < 1)
+            return;
+
+        if (!listEvent[0].CheckEventState())
+            return;
+
+        curText.text = listEvent[0].EventSuccesed();
+        listEvent.RemoveAt(0);
+
+    }
+
+
+    public void TouchBlockOnOff(bool isOn)
+    {
+        touchBlockGroup.blocksRaycasts = isOn;
+        touchBlockGroup.alpha = touchBlockGroup.blocksRaycasts ? 1.0f : 0.0f;
+    }
+
+
+
+   
 }
