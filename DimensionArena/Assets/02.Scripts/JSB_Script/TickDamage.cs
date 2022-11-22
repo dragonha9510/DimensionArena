78bf5a061b04032a1237d8863d68e54a66d2a65b
DEV_JSB <mvat1225@gmail.com>
2022-11-22 21:16:32 +0900

    20221122
    게임이 터지는데요
    하지만 해냈죠?

--- DimensionArena/Assets/02.Scripts/JSB_Script/TickDamage.cs
+++ DimensionArena/Assets/02.Scripts/JSB_Script/TickDamage.cs
@@ -22,6 +22,10 @@ public class TickDamage : MonoBehaviourPun
     public Dictionary<string , bool> willDamageApply = new Dictionary<string, bool>();
     public Dictionary<string,bool> WillDamageApply { get { return willDamageApply; } }
 
+    public Dictionary<GameObject, bool> willDamageApplyBox = new Dictionary<GameObject, bool>();
+    public Dictionary<GameObject, bool> WillDamageApplyBox { get { return willDamageApplyBox; } }
+
+
     public string OwnerID;
     public float ultimatePoint;
 
@@ -44,6 +48,38 @@ public class TickDamage : MonoBehaviourPun
       
     }
 
+    public void DamageApplyItemBox(GameObject itemBox, float damage)
+    {
+        if (isPercent)
+            itemBox.GetComponent<ItemBox>().GetTickDamage(true,damage);
+        else
+            itemBox.GetComponent<ItemBox>().GetTickDamage(false, damage);
+    }
+
+    IEnumerator InDamageZone(GameObject obj, float time, float damage)
+    {
+        if (startDamage)
+        {
+            DamageApplyItemBox(obj,damage);
+        }
+
+        while (true)
+        {
+            // 이 부분 수정하기 , 나가면 바로 꺼지지 않고 , time 만큼 기다렸다가 데미지를 입음
+            if (false == willDamageApplyBox[obj])
+            {
+                willDamageApplyBox.Remove(obj);
+                yield break;
+            }
+            yield return new WaitForSeconds(time);
+
+            if (false == willDamageApplyBox[obj])
+                continue;
+
+            DamageApplyItemBox(obj, damage);
+
+        }
+    }
     IEnumerator InDamageZone(string userID, float time, float damage)
     {
         if (startDamage)
@@ -93,10 +129,24 @@ public class TickDamage : MonoBehaviourPun
                     willDamageApply[other.name] = false;
                 }
             }
+            else if (other.gameObject.tag == "Item_Box")
+            {
+                if (true == isNestingCollision)
+                {
+                    willDamageApplyBox[other.gameObject] = false;
+                }
+                else if (false == isNestingCollision && false == IsInOtherObject(other.gameObject))
+                {
+                    willDamageApplyBox[other.gameObject] = false;
+                }
+            }
         }
         else
         {
-            willDamageApply[other.name] = false;
+            if(other.gameObject.tag == "Player")
+                willDamageApply[other.name] = false;
+            else if (other.gameObject.tag == "Item_Box")
+                willDamageApplyBox[other.gameObject] = false;
         }
     }
 
@@ -104,13 +154,20 @@ public class TickDamage : MonoBehaviourPun
     {
         foreach(GameObject obj in PartnerObject)
         {
-            Dictionary<string, bool> test = obj.GetComponent<TickDamage>().willDamageApply;
-            if (true == obj.GetComponent<TickDamage>().willDamageApply.ContainsKey(name))
+            if (true == obj.GetComponent<TickDamage>().WillDamageApply.ContainsKey(name))
                 return true;
         }
         return false;
     }
-
+    private bool IsInOtherObject(GameObject obj)
+    {
+        foreach (GameObject partner in PartnerObject)
+        {
+            if (true == partner.GetComponent<TickDamage>().WillDamageApplyBox.ContainsKey(obj))
+                return true;
+        }
+        return false;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -143,6 +200,25 @@ public class TickDamage : MonoBehaviourPun
                     StartCoroutine(InDamageZone(other.gameObject.name, damageTime, tickDamage));
                 }
             }
+            if(other.CompareTag("Item_Box"))
+            {
+                if (true == isNestingCollision)
+                {
+                    if (willDamageApplyBox.ContainsKey(other.gameObject))
+                    {
+                        willDamageApplyBox[other.gameObject] = true;
+                        return;
+                    }
+                    willDamageApplyBox.Add(other.gameObject, true);
+                    StartCoroutine(InDamageZone(other.gameObject, damageTime, tickDamage));
+                }
+                else if (false == isNestingCollision && false == IsInOtherObject(other.gameObject) && false == willDamageApplyBox.ContainsKey(other.gameObject))
+                {
+                    Debug.Log("데미지 중첩아님 코루틴 시작할꺼야");
+                    willDamageApplyBox.Add(other.gameObject, true);
+                    StartCoroutine(InDamageZone(other.gameObject, damageTime, tickDamage));
+                }
+            }
         }
         else
         {
@@ -153,13 +229,11 @@ public class TickDamage : MonoBehaviourPun
                 if (true == isNestingCollision)
                 {
                     willDamageApply.Add(other.name, true);
-
                     StartCoroutine(InDamageZone(other.gameObject.name, damageTime, tickDamage));
                 }
                 else if (false == isNestingCollision && false == IsInOtherObject(other.gameObject.name))
                 {
                     willDamageApply.Add(other.name, true);
-
                     StartCoroutine(InDamageZone(other.gameObject.name, damageTime, tickDamage));
                 }
             }
